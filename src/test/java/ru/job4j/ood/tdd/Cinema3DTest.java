3b842e42ba71afea90b786cf24d267ddb1d94519
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2023-02-16 11:51:46 +0300

    Что такое TDD? [#4918] - More tests added

--- src/test/java/ru/job4j/ood/tdd/Cinema3DTest.java
+++ src/test/java/ru/job4j/ood/tdd/Cinema3DTest.java
@@ -30,11 +30,52 @@ class Cinema3DTest {
     }
 
     @Test
+    void whenNoSuchSessionThenEmptyList() {
+        Cinema cinema = new Cinema3D();
+        List<Session> sessions = cinema.find(ses -> false);
+        assertNotNull(sessions);
+        assertTrue(sessions.isEmpty());
+    }
+
+    @Test
     void whenBuyOnInvalidRowThenGetException() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
         assertThrows(IllegalArgumentException.class, () -> cinema.buy(account, -1, 1, date));
+    }
+
+    @Test
+    void whenBuyOnInvalidColumnThenGetException() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        assertThrows(IllegalArgumentException.class, () -> cinema.buy(account, 1, -1, date));
+    }
+
+    @Test
+    void whenBuyOnInvalidAccountThenGetException() {
+        Account account = null;
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        assertThrows(IllegalArgumentException.class, () -> cinema.buy(account, 1, 1, date));
+    }
+
+    @Test
+    void whenPlaceOccupiedThenGetException() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        assertDoesNotThrow(() -> cinema.buy(account, 1, 1, date));
+        assertThrows(IllegalArgumentException.class, () -> cinema.buy(account, 1, 1, date));
+    }
+
+    @Test
+    void whenAddExistedSessionThenGetException() {
+        Cinema cinema = new Cinema3D();
+        Session session = new Session3D();
+        cinema.add(session);
+        assertThrows(IllegalArgumentException.class, () -> cinema.add(session));
     }
 
 }
