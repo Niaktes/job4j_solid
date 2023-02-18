b6f206d494991f7001e2dbe4ce8f1b7868f14804
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2023-02-18 20:36:39 +0300

    Отчеты. [#850] - Frames for lesson example added.

---
+++ src/main/java/ru/job4j/ood/srp/currency/Currency.java
@@ -0,0 +1,9 @@
+package ru.job4j.ood.srp.currency;
+
+public enum Currency {
+
+    RUB,
+    USD,
+    EUR
+
+}
---
+++ src/main/java/ru/job4j/ood/srp/currency/InMemoryCurrencyConverter.java
@@ -0,0 +1,21 @@
+package ru.job4j.ood.srp.currency;
+
+public class InMemoryCurrencyConverter implements CurrencyConverter {
+
+    private static final int CURRENCIES_COUNT = Currency.values().length;
+    private static double[][] conversationTable = new double[CURRENCIES_COUNT][CURRENCIES_COUNT];
+
+    public InMemoryCurrencyConverter() {
+        this.conversationTable[Currency.RUB.ordinal()][Currency.USD.ordinal()] = 0.014;
+        this.conversationTable[Currency.RUB.ordinal()][Currency.EUR.ordinal()] = 0.013;
+        this.conversationTable[Currency.USD.ordinal()][Currency.EUR.ordinal()] = 0.93;
+        this.conversationTable[Currency.USD.ordinal()][Currency.RUB.ordinal()] = 74D;
+        this.conversationTable[Currency.EUR.ordinal()][Currency.USD.ordinal()] = 1.07;
+        this.conversationTable[Currency.EUR.ordinal()][Currency.RUB.ordinal()] = 79.3;
+    }
+
+    @Override
+    public double convert(Currency source, double sourceValue, Currency target) {
+        return sourceValue * conversationTable[source.ordinal()][target.ordinal()];
+    }
+}
